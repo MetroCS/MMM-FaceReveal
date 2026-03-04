3ae50d11d74e6d93c2d8573484e5825e46484142
Jody Paul <jody@users.noreply.github.com>
2026-03-04 12:13:20 -0700

    Refactor ESLint configuration to use fully Flat constructs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,18 @@
 import css from "@eslint/css"
-import { defineConfig } from "eslint/config"
-import globals from "globals"
 import js from "@eslint/js"
 import markdown from "@eslint/markdown"
 import stylistic from "@stylistic/eslint-plugin"
+import globals from "globals"
 
-export default defineConfig([
-  { files: ["**/*.css"], plugins: { css }, language: "css/css", extends: ["css/recommended"] },
+export default [
+  {
+    files: ["**/*.css"],
+    plugins: { css },
+    language: "css/css",
+    rules: {
+      ...css.configs.recommended.rules,
+    }
+  },
   {
     files: ["**/*.js", "**/*.mjs"],
     languageOptions: {
@@ -16,16 +22,27 @@ export default defineConfig([
         ...globals.node,
         Log: "readonly",
         Module: "readonly",
+        config: "readonly",
       },
     },
     plugins: { js, stylistic },
-    extends: ["js/recommended", "stylistic/recommended"],
     rules: {
+      ...js.configs.recommended.rules,
+      ...stylistic.configs["recommended-extends"].rules,
       "@stylistic/brace-style": ["error", "1tbs", { allowSingleLine: true }],
       "@stylistic/comma-dangle": ["error", "only-multiline"],
       "@stylistic/max-statements-per-line": ["error", { max: 2 }],
-      "@stylistic/quotes": ["error", "double"]
+      "@stylistic/quotes": ["error", "double"],
+      "no-unused-vars": "warn",
+      "no-console": "off"
     }
   },
-  { files: ["**/*.md"], plugins: { markdown }, language: "markdown/gfm", extends: ["markdown/recommended"] },
-])
+  {
+    files: ["**/*.md"],
+    plugins: { markdown },
+    language: "markdown/gfm",
+    rules: {
+      ...markdown.configs.recommended.rules,
+    },
+  },
+];
